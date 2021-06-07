bfc4ed3042399c00de65b291af1e6c035815563c
mohammad alfayez <46851354+hmoodf@users.noreply.github.com>
2021-06-07 18:01:32 +0300

    Initial Commit

---
+++ my_project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  my_project
+//
+//  Created by mohammed alfayez on 07/06/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
